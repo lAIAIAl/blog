7475eb3ba24bec9be932f1373d4781e18f89ac7a
lAIAIAl <973494817@qq.com>
2021-10-09 22:32:19 +0800

    web bgm

---
+++ src/components/audio/audio.jsx
@@ -0,0 +1,21 @@
+import bgm from '../../assets/audio/Journey.flac'
+import { useEffect,useRef,Fragment } from 'react'
+
+import './audio.less'
+
+function Audio(){
+    const ref = useRef('sound')
+    useEffect(() => {
+        setTimeout(() => {
+            ref.current.play().catch(()=>{})
+        }, 5000);
+    }, [])
+    return (
+    <>
+        <audio ref={ref} preload="auto" controls="1" loop="1">
+            <source src={bgm} type="audio/mp3"/>
+        </audio>
+    </>
+    )
+}
+export default Audio
