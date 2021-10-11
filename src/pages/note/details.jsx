085a122e641c5ceb830b10c26241fdbdc23fc133
lAIAIAl <973494817@qq.com>
2021-10-11 16:29:33 +0800

    eslint-disable-line

--- src/pages/note/details.jsx
+++ src/pages/note/details.jsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState,useEffect} from "react";
 
 import { decodeQuery } from "../../utils/tools";
 import { reqNote } from '../../api/apis';
@@ -6,19 +6,21 @@ import { reqNote } from '../../api/apis';
 export default function Details(props){
     const {id} = decodeQuery(props.location.search);
     let [content,setContent] = useState({tag:'',title:'',category:'',create_time:''})
-    async function getDetails(){
-        let details = await reqNote(id);
-        if(document.getElementById("body"))
-            document.getElementById("body").innerHTML = details['blog'][0]['notes'];
-        let title = details['blog'][0]['title'];
-        let category = details['blog'][0]['category'];
-        let tag = details['blog'][0]['tag'];
-        let create_time = details['blog'][0]['create_time'];
-        setContent({
-            tag,title,category,create_time
-        });
-    }
-    getDetails()
+    useEffect(() => {
+        async function getDetails(){
+            let details = await reqNote(id);
+            if(document.getElementById("body"))
+                document.getElementById("body").innerHTML = details['blog'][0]['notes'];
+            let title = details['blog'][0]['title'];
+            let category = details['blog'][0]['category'];
+            let tag = details['blog'][0]['tag'];
+            let create_time = details['blog'][0]['create_time'];
+            setContent({
+                tag,title,category,create_time
+            });
+        }
+        getDetails()
+    }, []); //eslint-disable-line react-hooks/exhaustive-deps
     return (
         <>
             <div className="context">
